e0179754f7f6e50375dac9f066799308ca2e28f4
Nicholas MacRoy <nmacroy@gmail.com>
2020-02-23 23:59:50 -0600

    ES6 Objects

---
+++ playground/es6-objects.js
@@ -0,0 +1,18 @@
+const name = 'Andrew'
+const userAge = 27
+
+const user = {
+    name,
+    userAge,
+    location: 'Chicago'
+}
+
+console.log(user)
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
